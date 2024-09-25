a2f11b6975dc49b8f2d8928106f5139dab07dbda
MaxMatarrodonaLlopart <matarrodonamax@gmail.com>
2024-09-25 16:59:06 +0200

    p2 v1 falta 11 i 13

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,6 @@
 function confirmaRegistre(){
-    //completa
+    alert("Registrant usuari")
+    console.log("Registrant usuari")
+    let tagDiv = document.getElementById("formDiv")
+    tagDiv.innerHTML = "<p class='important'>Fet</p>"
 }
